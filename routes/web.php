7a66a556b9bd8ca1cc5d53cc891c5ce7c506aa3b
unknown <rabieabo2@gmail.com>
2025-06-27 14:11:25 +0300

    sdkskdkddfdfdfs

--- routes/web.php
+++ routes/web.php
@@ -39,15 +39,3 @@ Route::middleware(['web-api', 'auth:sanctum'])->group(function () {
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
-// عرض صفحة الدردشة
-Route::middleware(['auth:sanctum'])->get('/chat/{receiver_id}', [ChatController::class, 'index'])->name('chat.index');
-
-Route::middleware(['web-api', 'auth:sanctum'])->group(function () {
-    Route::post('/messages', [ChatController::class, 'send']);
-    Route::get('/messages/{userId}', [ChatController::class, 'fetch']);
-});
-
-
-
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);

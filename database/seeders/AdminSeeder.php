d258102cd1471ac89628f42e1535b6ae39614c56
mohammed Rayyan1 <rayyany831@gmail.com~>
2025-08-12 12:51:53 +0300

    complete Ai

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Employee;
+use App\Models\Profile;
 use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,7 +21,14 @@ class AdminSeeder extends Seeder
             'name' => 'Super Admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('12345678'),
-            'role_id' => Role::where('name', 'admin')->value('id') 
+            'role_id' => Role::where('name', 'admin')->value('id')
+        ]);
+        Profile::create([
+            'user_id' => 1,
+            'address' => '123 Main St',
+            'phone' => '0123456789',
+            'scientificLevel' => 'Bachelor of Law',
+            'age' => rand(25, 50),
         ]);
         User::create([
             'name' => 'Lawyer1',
@@ -28,11 +36,25 @@ class AdminSeeder extends Seeder
             'password' => bcrypt('12345678'),
             'role_id' => 5 // Lawyer role
         ]);
+        Profile::create([
+            'user_id' => 2,
+            'address' => '123 Main St',
+            'phone' => '0123456789',
+            'scientificLevel' => 'Bachelor of Law',
+            'age' => rand(25, 50),
+        ]);
         User::create([
             'name' => 'HR',
             'email' => 'hr1@gmail.com',
             'password' => bcrypt('12345678'),
             'role_id' => 3 // HR role
+        ]);
+        Profile::create([
+            'user_id' => 3,
+            'address' => '123 Main St',
+            'phone' => '0123456789',
+            'scientificLevel' => 'Bachelor of Law',
+            'age' => rand(25, 50),
         ]);
         User::create([
             'name' => 'acountant',
@@ -41,7 +63,15 @@ class AdminSeeder extends Seeder
             'role_id' => 4 // acountant role
         ]);
 
-                Employee::updateOrCreate(
+        Profile::create([
+            'user_id' => 4,
+            'address' => '123 Main St',
+            'phone' => '0123456789',
+            'scientificLevel' => 'Bachelor of Law',
+            'age' => rand(25, 50),
+        ]);
+
+        Employee::updateOrCreate(
             ['id' => 1], // لتفادي التكرار عند إعادة تشغيل seeder
             [
                 'salary' => 500,
@@ -50,7 +80,7 @@ class AdminSeeder extends Seeder
                 'user_id' => 3,
             ]
         );
-                Employee::updateOrCreate(
+        Employee::updateOrCreate(
             ['id' => 2], // لتفادي التكرار عند إعادة تشغيل seeder
             [
                 'salary' => 1000,
